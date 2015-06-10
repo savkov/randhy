8501c87d755f8c6c8b98138659892faf2c0de60a
Aleksandar Savkov <cytehuop@gmail.com>
2015-06-10 09:40:16 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'Aleksandar Savkov'
+
+from distutils.core import setup
+
+setup(name='randhy',
+      version='1.0',
+      description='Randomisation hypothesis testing library',
+      author='Aleksandar Savkov',
+      author_email='aleksandar@savkov.eu',
+      url='https://www.github.com/asavkov/randhy/',
+      package_dir={'': 'src'},
+      packages=['']
+     )
