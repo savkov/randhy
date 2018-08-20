190ea2312e171ddf6a52af6bd3178de6ee57e1fe
Sasho Savkov <me@sasho.io>
2018-08-20 03:26:38 +0100

    version from file

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
+version = open('VERSION').read()
 
 __author__ = 'Sasho Savkov'
 __credits__ = ["William Morgan"]
 __license__ = "MIT"
-__version__ = "2.0.0"
+__version__ = version
 __email__ = "me@sasho.io"
 __status__ = "Production"
 
 
 setup(
       name='randhy',
-      version='2.0.0',
+      version=version,
       description='Approximate randomisation library',
       author='Sasho Savkov',
       author_email='me@sasho.io',
