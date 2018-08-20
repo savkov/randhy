64611c75a1c9bd5c4f725d1d8be2aa56ac90f1c0
Sasho Savkov <me@sasho.io>
2018-08-20 03:53:42 +0100

    long description

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 version = open('VERSION').read()
+long_description = open('README.md').read()
 
 __author__ = 'Sasho Savkov'
 __credits__ = ["William Morgan"]
@@ -11,12 +12,14 @@ __status__ = "Production"
 
 
 setup(
-      name='randhy',
-      version=version,
-      description='Approximate randomisation library',
-      author='Sasho Savkov',
-      author_email='me@sasho.io',
-      url='https://www.github.com/asavkov/randhy/',
-      package_dir={'': 'src'},
-      packages=['randhy']
+    name='randhy',
+    version=version,
+    description='Approximate randomisation library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    author='Sasho Savkov',
+    author_email='me@sasho.io',
+    url='https://www.github.com/asavkov/randhy/',
+    package_dir={'': 'src'},
+    packages=['randhy']
 )
